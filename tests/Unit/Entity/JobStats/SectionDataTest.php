9e6797ff9130855c7077db944dce1ca0bcfb810e
DaWen <dan.wendlandt@gmail.com>
2016-04-22 08:45:29 +0200

    Created test for entity SectionData

--- tests/Unit/Entity/JobStats/SectionDataTest.php
+++ tests/Unit/Entity/JobStats/SectionDataTest.php
@@ -46,4 +46,76 @@ class SectionDataTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($dateTime > $this->sectionData->getCreatedAt());
     }
 
+    public function testGetSetName()
+    {
+        $name = 'my-name';
+
+        $this->assertNull($this->sectionData->getName());
+        $this->sectionData->setName($name);
+        $this->assertSame($name, $this->sectionData->getName());
+    }
+
+    public function testGetSetTimeTaken()
+    {
+        $timeTaken = 1700;
+
+        $this->assertSame(0, $this->sectionData->getTimeTaken());
+        $this->sectionData->setTimeTaken($timeTaken);
+        $this->assertSame($timeTaken, $this->sectionData->getTimeTaken());
+    }
+
+    public function testGetSetMemoryUsage()
+    {
+        $memoryUsage = 1100000;
+
+        $this->assertSame(0, $this->sectionData->getMemoryUsage());
+        $this->sectionData->setMemoryUsage($memoryUsage);
+        $this->assertSame($memoryUsage, $this->sectionData->getMemoryUsage());
+    }
+
+    public function testGetSetMemoryUsed()
+    {
+        $memoryUsed = 11000234;
+
+        $this->assertSame(0, $this->sectionData->getMemoryUsed());
+        $this->sectionData->setMemoryUsed($memoryUsed);
+        $this->assertSame($memoryUsed, $this->sectionData->getMemoryUsed());
+    }
+
+    public function testGetSetOptions()
+    {
+        $options = ['my' => 'option'];
+
+        $this->assertTrue(is_array($this->sectionData->getOptions()));
+        $this->assertEmpty($this->sectionData->getOptions());
+        $this->sectionData->setOptions($options);
+        $this->assertSame($options, $this->sectionData->getOptions());
+    }
+
+    public function testToArray()
+    {
+        $name = 'my-name';
+        $timeTaken = 1700;
+        $memoryUsage = 1100000;
+        $memoryUsed = 11000234;
+        $options = ['my' => 'option'];
+
+        $arrayData = array(
+            'name' => $name,
+            'timeTaken' => $timeTaken,
+            'memoryUsage' => $memoryUsage,
+            'memoryUsed' => $memoryUsed,
+            'options' => $options,
+            'createdAt' => $this->sectionData->getCreatedAt()->format('Y-m-d H:i:s')
+        );
+
+        $this->sectionData->setName($name);
+        $this->sectionData->setTimeTaken($timeTaken);
+        $this->sectionData->setMemoryUsage($memoryUsage);
+        $this->sectionData->setMemoryUsed($memoryUsed);
+        $this->sectionData->setOptions($options);
+
+        $this->assertSame($arrayData, $this->sectionData->toArray());
+    }
+
 }
